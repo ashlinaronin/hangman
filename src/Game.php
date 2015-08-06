8a8ca4010abc37dd22d7daacf670b57ad76ce96c
Ashlin Aronin & Ike Mattice <student@epicodus.com>
2015-08-06 14:26:29 -0700

    Create Game class

---
+++ src/Game.php
@@ -0,0 +1,60 @@
+<?php
+class Game {
+    private $word;
+    private $guessesLeft;
+    private $guessesMade;
+
+    function __construct()
+    {
+        private $wordOptions = array(
+            "chicken",
+            "robot",
+            "filth",
+            "doll",
+            "spinlister");
+
+        // choose random word
+        $this->word = $wordOptions[array_rand($wordOptions)];
+        $this->guessesLeft = 6;
+        $this->guessesMade = 0;
+    }
+
+    // setters
+    function setWord ($new_word)
+    {
+        $this->word = (string) $new_word;
+    }
+    function setGuessesLeft ($new_left)
+    {
+        $this->guessesLeft = (int) $new_left;
+    }
+    function setGuessesMade ($new_made)
+    {
+        $this->guessesMade = (int) $new_made;
+    }
+
+    // getters
+    function getWord()
+    {
+        return $this->word;
+    }
+    function getGuessesLeft()
+    {
+        return $this->guessesLeft;
+    }
+    function getGuessesMade()
+    {
+        return $this->guessesMade;
+    }
+
+    //save function to store current game in cookies
+    function save()
+    {
+        $_SESSION['current_game'] = $this;
+    }
+    // reset game status to a new game
+    function restart()
+    {
+        $_SESSION['current_game'] = new Game();
+    }
+}
